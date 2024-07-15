85166a787f01be6458a47bb1c23d58462d3fae20
East_Sparkle <87969415+Insazor@users.noreply.github.com>
2024-07-16 00:07:01 +0900

    Add files via upload

--- crawling/naver.py
+++ crawling/naver.py
@@ -2,6 +2,10 @@ import os
 import sys
 import urllib.request
 
+from selenium.common import NoSuchElementException
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
 client_id = "_C12LIJCwlme2ozPrU3k" #네이버 오픈 API ID
 client_secret = "Gehzb9ohra" #네이버 오픈 API secret
 
@@ -50,10 +54,34 @@ print('\n<<링크 모음>>')
 print(links)
 print('총 링크 수: ',len(links),'개')
 
-blog_links = []
-for i in links:
-    a = i.replace('\\','')
-    b = a.replace('?Redirect=Log&logNo=','/')
+import time
+from selenium import webdriver # 동적 사이트 수집
+from webdriver_manager.chrome import ChromeDriverManager # 크롬 드라이버 설치
+from selenium.webdriver.chrome.service import Service # 자동적 접근
+from selenium.webdriver.chrome.options import Options # 크롭 드라이버 옵션 지정
+from selenium.webdriver.common.by import By # find_element 함수 쉽게 쓰기 위함
+from selenium.webdriver.common.keys import keys
 
-print(blog_links)
-print('생성된 링크 갯수 ; ', len(blog_links),'개')
+driver= webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.implicitly_wait(3)
+
+contents = []
+for i in links:
+    #블로그 링크 하나씩 불러오기
+    driver.get(i)
+    time.sleep(1)
+    #블로그 안 본문이 있는 iframe에 접근하기
+    driver.switch_to.frame("mainFrame")
+    #본문 내용 크롤링하기
+    try:
+        a = driver.find_element(By.CSS_SELECTOR,'div.se-main-container').text
+        contents.append(a)
+    # NoSuchElement 오류시 예외처리(구버전 블로그에 적용)
+    except NoSuchElementException:
+        a = driver.find_element(By.CSS_SELECTOR,'div#content-area').text
+        contents.append(a)
+
+    print('본문: \n', a)
+
+driver.quit()
+print("<<본문 크롤링 완료.>>")
